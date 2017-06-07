77b33000ff363a6c22407eb3e2f3efba9700e802
Mosen <mosen1@gmail.com>
2017-06-07 16:14:51 +1000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="apkg",
+    version="0.1",
+    description="Support for the Apple Distribution Package format",
+    packages=['apkg'],
+    author="mosen",
+    license="MIT",
+    url="https://github.com/mosen/apkg",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6'
+    ],
+    keywords='apple package pkg',
+    install_requires=[
+    ],
+    python_requires='>=3.3',
+    tests_require=[
+        'pytest',
+        'mock'
+    ],
+    setup_requires=['pytest-runner'],
+    entry_points={
+        'console_scripts': [
+        ]
+    },
+    zip_safe=False
+)
+
+
